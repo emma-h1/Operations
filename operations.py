918235c6c25a73c836e6ddec58313a3b6dc04d95
Emma <esheiser@loyola.edu>
2021-09-18 14:06:25 -0400

    commit1

---
+++ operations.py
@@ -0,0 +1,14 @@
+import math
+
+a=5
+b=4
+c=2
+
+print("a//c = ", a//c)
+#// is an integer division
+print("a%b = ", a%b)
+# % is the remainder of division
+print("b**c = ", b**c)
+print("b * = c = ", b * = c)
+
+#do back of chapter 1 exercises
